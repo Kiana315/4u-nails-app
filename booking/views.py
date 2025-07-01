dd6d8427adbb7b768eeaa021964aee8f18d8508f
Liz <93419255+Kiana315@users.noreply.github.com>
2025-06-30 19:19:56 -0600

    Create API

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Appointment
+from .serializers import AppointmentSerializer
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all().order_by('-created_at')
+    serializer_class = AppointmentSerializer
